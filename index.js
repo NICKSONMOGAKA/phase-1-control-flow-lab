0d8882dac98fe4f2fd8db58dbe6fe24a26e90f1b
+ <nickson.mogaka@student.moringaschool.com>
2024-07-05 10:55:06 +0300

    modify

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-function scuberGreetingForFeet(distance) {
-  if (distance <= 400) {
-      return 'This one is on me!';
-  } else if (distance > 400 && distance <= 2000) {
-      return 'That will be twenty bucks.';
-  } else if (distance > 2000 && distance <= 2500) {
-      return 'I will gladly take your thirty bucks.';
-  } else {
-      return 'No can do.';
+function scuberGreetingForFeet(number) {
+    // Write your code here!
+    if (number < 400) {
+      return "This one is on me!";
+    } else if (number > 2000) {
+      if (number > 2500) {
+        return "No can do.";
+      } else return "I will gladly take your thirty bucks.";
+    }
   }
-}
-
-function ternaryCheckCity(city) {
-  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
-}
-
-function switchOnCharmFromTip(tip) {
-  switch(tip) {
-      case 'generous':
-          return 'Thank you so much.';
-      case 'not as generous':
-          return 'Thank you.';
+  // console.log(scuberGreetingForFeet(2501));
+  
+  function ternaryCheckCity(city) {
+    // Write your code here!
+    return city === "NYC" ? "Ok, sounds good." : "No go.";
+  }
+  
+  function switchOnCharmFromTip(tip) {
+    // Write your code here!
+    switch (tip) {
+      case "generous":
+        return "Thank you so much.";
+        break;
+      case "not as generous":
+        return "Thank you.";
+  
       default:
-          return 'Bye.';
+        return "Bye.";
+        break;
+    }
   }
-}
